a51f0f6d6171f3523276479faee9bc7d3098d129
Bhatkiran74 <bhatkiran74@gmail.com>
2020-10-01 23:35:22 +0530

    third commit done by kiran bhat

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+    printf("\nEnter two numbers...:  ");
+    scanf("%d%d", &a, &b);
+    printf("\nAdditon %d & %d is : %d", a, b, a + b);
+
+    return 0;
+}
