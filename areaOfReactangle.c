6e8f82c2ed72e441af9f1e855fcc939a9f13e41a
Bhatkiran74 <bhatkiran74@gmail.com>
2020-10-01 23:49:14 +0530

    write a program to find area of reactangle

---
+++ areaOfReactangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float breadth, width;
+    printf("\nENter breadth and width of Reactangle : ");
+    scanf("%f%f", &breadth, &width);
+
+    printf("\nArea of Reactangle is : %f", breadth * width);
+
+    return 0;
+}
